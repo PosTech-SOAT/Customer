f669875b8b8e319a7d9ac484df4d928bc9d56fa9
Bruno Henrique <bruno.hsn6@gmail.com>
2024-08-08 22:26:03 -0300

    feat: creation of lgpd services

---
+++ src/domain/entities/LGPDForm.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+import { LGPDFormDto } from '../../infra/dto/LGPDFormDto';
+
+export type ILGPDForm = {
+	_id: string;
+	clientId: string;
+} & LGPDFormDto &
+	Document;
+
+const lgpdFormSchema: Schema = new Schema(
+	{
+		_id: { type: String, default: uuidv4 },
+		cliendId: { type: String, default: uuidv4 },
+		userId: { type: String, required: true },
+		name: { type: String, required: true },
+		address: { type: String, required: true },
+		phone: { type: String, required: true },
+		cpf: { type: String, required: true },
+		exclusion: { type: Boolean, required: true },
+	},
+	{ timestamps: true },
+);
+
+export const LgpdFormModel = mongoose.model<ILGPDForm>(
+	'LGPDForm',
+	lgpdFormSchema,
+);
--- src/infra/controllers/ClientController.ts
+++ src/infra/controllers/ClientController.ts
@@ -5,9 +5,12 @@ import ClientListUseCase from '../../domain/use-cases/Client/ClientListUseCase';
 import ClientFindOneByCpfUseCase from '../../domain/use-cases/Client/ClientFindOneByCpfUseCase';
 import ClientUpdateUseCase from '../../domain/use-cases/Client/ClientUpdateUseCase';
 import ClientFindOneByIdUseCase from '../../domain/use-cases/Client/ClientFindOneByIdUseCase';
+import { LGPDFormDto } from '../dto/LGPDFormDto';
+import { IClient } from '../entities/ClientEntity';
+import { isUUID } from '../../utils/StringUtils';
+import ClientLgpdUseCase from '../../domain/use-cases/Client/ClientLgpdUseCase';
 
 export default class ClientController {
-
 	async create(request: Request, response: Response) {
 		const { name, email, cpf } = request.body;
 
@@ -20,7 +23,9 @@ export default class ClientController {
 		try {
 			await createClientUseCase.execute({ name, email, cpf });
 
-			return response.status(201).json({ message: 'Client created successfully' });
+			return response
+				.status(201)
+				.json({ message: 'Client created successfully' });
 		} catch (error: any) {
 			return response.status(400).json({ message: error.message });
 		}
@@ -66,8 +71,39 @@ export default class ClientController {
 		const clientListUseCase = container.resolve(ClientUpdateUseCase);
 
 		try {
-			await clientListUseCase.execute({id: request.params.cpf, params: request.body});
-			return response.status(200).json({ message: 'Client updated successfully' });
+			await clientListUseCase.execute({
+				id: request.params.cpf,
+				params: request.body,
+			});
+			return response
+				.status(200)
+				.json({ message: 'Client updated successfully' });
+		} catch (error: any) {
+			return response.status(400).json({ message: error.message });
+		}
+	}
+
+	async lgpd(request: Request, response: Response) {
+		try {
+			const form: LGPDFormDto = request.body;
+			let client: IClient | null;
+			if (isUUID(form.userId)) {
+				client = await container
+					.resolve(ClientFindOneByIdUseCase)
+					.execute(form.userId);
+			} else {
+				client = await container
+					.resolve(ClientFindOneByCpfUseCase)
+					.execute(form.userId);
+			}
+			if (!client) {
+				throw new Error('Client not found!');
+			}
+			const clientLgpdUseCase = container.resolve(ClientLgpdUseCase);
+			await clientLgpdUseCase.execute({ client, form });
+			return response
+				.status(200)
+				.json({ message: 'Client updated successfully' });
 		} catch (error: any) {
 			return response.status(400).json({ message: error.message });
 		}
